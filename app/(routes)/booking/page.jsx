c4ea6388c595b868cf850ca5284f46cc3a796f5e
DennisKoskei <89400168+DennisKoskei@users.noreply.github.com>
2023-11-18 16:27:50 +0300

    Added static code passenger details to bookings page

--- app/(routes)/booking/page.jsx
+++ app/(routes)/booking/page.jsx
@@ -20,13 +20,50 @@ const BookingPage = () => {
                             {/* <Image src></Image> */}
                             image of bus
                         </div>
-                        <div className="w-3/4 bg-green-300 rounded-xl p-2">
-                            Details of the bus such as To, From, Departure time,
-                            Price
+                        <div className="flex flex-wrap gap-4 w-3/4 bg-green-300 rounded-xl p-2">
+                            <h3>Nairobi - Kericho</h3>
+                            <h3>Departure</h3>
+                            <h3>Arrival</h3>
+                            <p>10:00 AM</p>
+                            <p>04:00 AM</p>
+                            <h3>Price</h3>
+                            <p>1500 /=</p>
                         </div>
                     </div>
                     <div className="w-full p-2 mt-4 bg-green-300 rounded-xl">
-                        seats selected
+                        <h1 className="my-4">Booking Submission</h1>
+                        <div className="flex flex-col h-40 justify-between">
+                            <form action="/" method="post">
+                                <div className="flex flex-nowrap gap-2">
+                                    <input
+                                        type="text"
+                                        name=""
+                                        readOnly="true"
+                                        id="seatId"
+                                        defaultValue={"seat"}
+                                        cols="5"
+                                    />
+                                    <input type="text" name="firstName" id="" />
+                                    <input type="text" name="LastName" id="" />
+                                    <input
+                                        type="checkbox"
+                                        name="remove"
+                                        id="removeBtn"
+                                        checked="false"
+                                    />
+                                </div>
+                                <div className="flex gap-2 justify-end justify-items-end">
+                                    <h3>Total</h3>
+                                    <p className="text-xl font-bold">3000 /=</p>
+                                    <button
+                                        className="text-l font-semibold bg-green-200 rounded-md mt-4 px-4 py-2"
+                                        type="submit"
+                                    >
+                                        Make Payment
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
