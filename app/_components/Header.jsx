f961356818bd3db80b6058e242d500b1a0eacf9b
DennisKoskei <89400168+DennisKoskei@users.noreply.github.com>
2023-11-19 21:09:26 +0300

    Modified container class in code

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="flex flex-row p-4 bg-green-500 gap-2">
+        <div className="flex flex-row py-4 px-11 bg-green-500 gap-2">
             <div className="w-1/2 flex-row font-extrabold text-2xl items-center gap-4">
                 <Image
                     className="rounded-full"
@@ -17,7 +17,7 @@ const Header = () => {
                 </h1>
             </div>
             <div className="w-1/2 flex flex-row items-end justify-end gap-2 pr-8 text-lg font-normal">
-                <div className="flex flex-row w-5/6 h-full items-center ">
+                <div className="flex flex-row w-5/6 h-full items-center justify-center ">
                     <ul className="flex-row gap-4 pr-2">
                         <li>
                             <Link href="/about"> About Us</Link>
@@ -33,13 +33,15 @@ const Header = () => {
                         </li>
                     </ul>
                 </div>
-                <div className="w-1/6">
+                <div className="flex flex-row w-1/6 items-center gap-2">
+                    <p>p</p>
                     <Image
                         className="rounded-full"
                         src="/Assets/Eren Yaeger- Screenshot.png"
                         width={50}
                         height={50}
                     />
+                    <p>p</p>
                 </div>
             </div>
         </div>
